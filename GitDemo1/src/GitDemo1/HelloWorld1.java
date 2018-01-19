aac1b4e69c2d5dd1d11b4418a2fd79932720bf8d
Nivrutti Chaudhari <nivsoftest@gmail.com>
2018-01-19 19:52:34 +0530

    initial commit - gitDemo1

---
+++ GitDemo1/src/GitDemo1/HelloWorld1.java
@@ -0,0 +1,19 @@
+package GitDemo1;
+
+public class HelloWorld1 {
+
+	public static void main(String[] args) {
+		String h = "Hello";
+		String w = "World";
+		int i = 1;
+		
+		System.out.println("Output: "+h+w+i);
+		System.out.println();
+		System.out.println("First Line..");
+		System.out.println("More TO DO...");
+		System.out.println(".");
+		System.out.println("Last line - END!");
+
+	}
+
+}
